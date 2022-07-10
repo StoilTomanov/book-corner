617e1a0abd3c0966d3a85291c3350214ff9107aa
StoilTomanov <stoiltomanov@gmail.com>
2022-07-10 12:09:43 +0300

    created-home-component

---
+++ src/components/App/Home/Home.js
@@ -0,0 +1,18 @@
+export const Home = () => {
+    return (
+        <div id="bgslider">
+        <figure>
+            <img src="./assets//backgrounds/10056355054_14e91113d8_b.jpg" alt="Slider" />
+            <img src="./assets//backgrounds/2019-11-28-12-57-56-1200x800.jpg" alt="Slider" />
+            <img src="./assets//backgrounds/8063774310_74036c6632_b.jpg" alt="Slider" />
+            <img src="./assets//backgrounds/8314929977_28fd740070_b.jpg" alt="Slider" />
+            <img src="./assets//backgrounds/upwk61805267-wikimedia-image.jpg" alt="Slider" />
+        </figure>
+        <div class="home-intro">
+            <p>Book Corner is the right place you need to be. This is your spot. The place where you are surraounded by thousands of books. With the size of over 1100 m2 you can easyly get lost in the remarcable world of the book. Since you are here, why
+                not have a look at our catalog.</p>
+            <button>Catalog</button>
+        </div>
+    </div>
+    );
+}
