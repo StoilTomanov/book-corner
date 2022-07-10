db7c6aa1ead9ad90bfedce03938feaba069c8f73
StoilTomanov <stoiltomanov@gmail.com>
2022-07-10 11:47:51 +0300

    created-footer-component

---
+++ src/components/App/Footer/Footer.js
@@ -0,0 +1,29 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+export const Footer = () => {
+    return (
+        <footer id="footer">
+        <nav className="footer-nav">
+            <ul>
+                <li className="nav-footer-element">
+                    <a href="#" className="nav-link">About us</a>
+                </li>
+                <li className="nav-footer-element">
+                    <a href="#" className="nav-link">Terms and Conditions</a>
+                </li>
+                <li className="nav-footer-element">
+                    <a href="#" className="nav-link">Privacy Policy</a>
+                </li>
+                <li className="nav-footer-element">
+                    <a href="#" className="nav-link">Delivery</a>
+                </li>
+                <li className="nav-footer-element">
+                    <a href="#" className="nav-link">Contacts</a>
+                </li>
+                <li className="nav-footer-element">
+                    <a href="#" className="nav-link">FAQ</a>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+    );
+}
