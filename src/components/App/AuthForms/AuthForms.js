ebd11a3a179a0c1df2ba6baafa3bb98a387d401a
StoilTomanov <stoiltomanov@gmail.com>
2022-07-12 21:48:51 +0300

    auth-forms

---
+++ src/components/App/AuthForms/AuthForms.js
@@ -0,0 +1,37 @@
+export const AuthForms = () => {
+    return (
+        <section className="forms-page section">
+            <div className="form-controls">
+                <button className="login">
+                    Login
+                </button>
+                <button className="register">
+                    Register
+                </button>
+            </div>
+            <div className="line-divider"></div>
+            <div className="forms-c ontainer">
+            <div className="login-element">
+                    <form className="login-form">
+                        <label htmlFor="username">Username</label>
+                        <input name="username" type="text" />
+                        <label htmlFor="password">Password</label>
+                        <input name="password" type="password" />
+                        <button className="submit-btn" type="submit">Login</button>
+                    </form>
+                </div>
+                <div className="register-element">
+                    <form className="register-form">
+                        <label htmlFor="username">Username</label>
+                        <input name="username" type="text" />
+                        <label htmlFor="password">Password</label>
+                        <input name="password" type="password" />
+                        <label htmlFor="re-password">Re-Password</label>
+                        <input name="re-password" type="password"/ >
+                        <button className="submit-btn" type="submit">Register</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/components/App/Home/Home.js
+++ src/components/App/Home/Home.js
@@ -1,6 +1,6 @@
 export const Home = () => {
     return (
-        <div id="bgslider">
+        <div id="image-slider">
         <figure>
             <img src="./assets//backgrounds/10056355054_14e91113d8_b.jpg" alt="Slider" />
             <img src="./assets//backgrounds/2019-11-28-12-57-56-1200x800.jpg" alt="Slider" />
@@ -8,8 +8,8 @@ export const Home = () => {
             <img src="./assets//backgrounds/8314929977_28fd740070_b.jpg" alt="Slider" />
             <img src="./assets//backgrounds/upwk61805267-wikimedia-image.jpg" alt="Slider" />
         </figure>
-        <div class="home-intro">
-            <p>Book Corner is the right place you need to be. This is your spot. The place where you are surraounded by thousands of books. With the size of over 1100 m2 you can easyly get lost in the remarcable world of the book. Since you are here, why
+        <div className="home-intro">
+            <p>Book Corner is the right place you need to be. This is your spot. The place where you are surrounded by thousands of books. With the size of over 1100 m2 you can easyly get lost in the remarcable world of the book. Since you are here, why
                 not have a look at our catalog.</p>
             <button>Catalog</button>
         </div>
