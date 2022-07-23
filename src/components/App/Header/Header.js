b1d86ba7ec23fbec337be1373d9ecde36e15f09a
StoilTomanov <stoiltomanov@gmail.com>
2022-07-23 17:25:44 +0300

    styled components

--- src/components/App/Header/Header.js
+++ src/components/App/Header/Header.js
@@ -1,18 +1,25 @@
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export const Header = () => {
+    const pathname = useLocation().pathname;
+
     return (
-        <header id="header">
-        <img className="logo-img" src="./assets/logo_transparent.png" alt="Book Corner logo" />
-        <nav className="header-nav">
-            <ul>
-                <li className="nav-header-element">
+        <header id={pathname === '/home' ? 'header' : 'small-header'}>
+            <img className={
+                pathname === '/home' ? "logo-img" : "smaller-logo-img"
+            } src={
+                pathname === '/home' ? "./assets/logo_transparent.png" : "./assets/smaller-logo-img.png"
+            } alt="Book Corner logo" />
+            <nav className="header-nav">
+                <ul>
+                    {/* <li className="nav-header-element">
                     <Link to="/" className="nav-link">Home</Link>
-                </li>
-                <li className="nav-header-element">
+                </li> */}
+                    {/* <li className="nav-header-element">
                     <Link to="/catalog" className="nav-link">Catalog</Link>
-                </li>
-                {/* <li className="nav-header-element">
+                </li> */}
+                    {/* <li className="nav-header-element">
                     <Link to="/newest" className="nav-link">Newest</Link>
                 </li>
                 <li className="nav-header-element">
@@ -21,14 +28,14 @@ export const Header = () => {
                 <li className="nav-header-element">
                     <Link to="/sale" className="nav-link">On Sale</Link>
                 </li> */}
-                <li className="nav-header-element">
-                    <Link to="/login" className="nav-link">Login</Link>
-                </li>
-                <li className="nav-header-element">
-                    <Link to="/register" className="nav-link">Register</Link>
-                </li>
-                {/* Logged user */}
-                <li className="nav-header-element">
+                    <li className="nav-header-element">
+                        <Link to="/login" className="nav-link">Login</Link>
+                    </li>
+                    <li className="nav-header-element">
+                        <Link to="/register" className="nav-link">Register</Link>
+                    </li>
+                    {/* Logged user */}
+                    {/* <li className="nav-header-element">
                     <Link to="/create" className="nav-link">Create</Link>
                 </li>
                 <li className="nav-header-element">
@@ -36,15 +43,15 @@ export const Header = () => {
                 </li>
                 <li className="nav-header-element">
                     <Link to="/profile" className="nav-link">Profile</Link>
-                </li>
-                {/* <li className="nav-header-element">
+                </li> */}
+                    {/* <li className="nav-header-element">
                     <Link to="/bin" className="nav-link">Bin</Link>
                 </li>
                 <li className="nav-header-element">
                     <Link to="/logout" className="nav-link">Logout</Link>
                 </li> */}
-            </ul>
-        </nav>
-    </header>
+                </ul>
+            </nav>
+        </header>
     );
 }
--- src/components/App/Home/Home.js
+++ src/components/App/Home/Home.js
@@ -1,17 +1,19 @@
+import { Link } from 'react-router-dom';
+
 export const Home = () => {
     return (
         <div id="image-slider">
         <figure>
-            <img src="./assets//backgrounds/10056355054_14e91113d8_b.jpg" alt="Slider" />
-            <img src="./assets//backgrounds/2019-11-28-12-57-56-1200x800.jpg" alt="Slider" />
-            <img src="./assets//backgrounds/8063774310_74036c6632_b.jpg" alt="Slider" />
             <img src="./assets//backgrounds/8314929977_28fd740070_b.jpg" alt="Slider" />
-            <img src="./assets//backgrounds/upwk61805267-wikimedia-image.jpg" alt="Slider" />
         </figure>
+        <div className="home-intro-text">
+            Get <span className="color-text">ready</span> to enrich your <span className="color-text">library</span> 
+        </div>
+        <div className="home-text-divider"></div>
         <div className="home-intro">
             <p>Book Corner is the right place you need to be. This is your spot. The place where you are surrounded by thousands of books. With the size of over 1100 m2 you can easily get lost in the remarkable world of the book. Since you are here, why
-                not have a look at our catalog.</p>
-            <button>Catalog</button>
+                don't you have a look at our online catalog and buy your new book.</p>
+                <Link to={'/catalog'}><button>Catalog</button></Link>
         </div>
     </div>
     );
