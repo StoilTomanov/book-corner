623c8e50e54e3d5f36485811f6c74ce27cd82c9f
StoilTomanov <stoiltomanov@gmail.com>
2022-07-23 21:18:53 +0300

    privacy page created

---
+++ src/components/App/Privacy/Privacy.js
@@ -0,0 +1,74 @@
+export const Privacy = () => {
+    return (
+        <section className="privacy-section">
+            <h2>
+                We care about your privacy! Please read below sections so you are familiar on how we use your data.
+            </h2>
+            <div className="divider-privacy"></div>
+            <div>
+                <h4>
+                    Collection and processing of personal data
+                </h4>
+                <p>
+                    Personal data is information that serves for your identification, i.e. name, telephone number, email or postal address. Book Corner does not collect personal data from you, unless you provide it in special cases, for example when you order products. By providing your personal data when registering in the online store, you agree to receive e-mail messages about current commercial offers, promotions, news and the organization of events and campaigns for marketing purposes.
+                </p>
+            </div>
+            <div>
+                <h4>
+                    Basis and purposes of processing the personal data collected for sending the newsletter:
+                </h4>
+                <p>
+                    Your personal data will be processed and stored by the Administrator on the following grounds: your express consent to process your personal data for the purposes and within the terms specified in these rules, as well as communication for the purpose of sending a commercial newsletter.
+                </p>
+                <p>
+                    Your personal data will be used by the Administrator on the territory of the Republic of Bulgaria and the same will not be transmitted to third parties, except to: the automated marketing platform MailChimp with a view to sending the newsletter; to the companies operating the relevant plug-in or social network that you choose to use yourself; as well as Google, in order to carry out web analysis. Only IP address and log information is provided to the latter in order to carry out the analysis.
+                </p>
+                <p>
+                    Your personal data will be stored in accordance with the legal requirements for the protection of personal data in electronic format in the following electronic register maintained by the Administrator: "Electronic Bulletin Register" for no longer than the minimum period necessary for this purpose after your unsubscribe.
+                </p>
+            </div>
+            <div>
+                <h4>
+                    Use of own cookies
+                </h4>
+                <p>
+                    In order to make visiting our website attractive and to enable the use of certain functions, we use so-called cookies on various pages. These are small text files that are stored on your device. Some of the cookies we use are deleted at the end of the browser session, i.e. after you close it (so-called session cookies). Other cookies remain on your device and allow us or our partner companies to recognize your browser on your next visit (so-called persistent cookies). Cookies cannot access other files on your computer or learn your email address. The legal basis for processing personal data when using cookies is Art. 6 paragraph 1 of the GDPR.
+                </p>
+                <p>
+                    Cookies are stored on your device and you can fully control their use. You can disable or limit the use of cookies by changing your internet browser settings. Already stored cookies can be deleted at any time. This can also be done automatically. If cookies are disabled for our website, you may not be able to use all features to their full extent.
+                </p>
+            </div>
+            <div>
+                <h4>
+                    Browser cookie control
+                </h4>
+                <p>
+                    If you subsequently do not want us to collect and analyze information about your visit, you can opt-out at any time.
+                </p>
+                <p>
+                    For the technical implementation of this refusal, the corresponding marker is set to Off in your browser. This cookie only serves to register your opt-out. Please note that for technical reasons cookies can only be used for the browser from which they are set. If you delete cookies or use a different browser or device, you must set your desired preferences again.
+                </p>
+            </div>
+            <div>
+                <h4>
+                    Technical protection measures
+                </h4>
+                <p>
+                    We go to great lengths to protect our customers, as well as our e-newsletter subscribers, from unauthorized access or unauthorized modification, disclosure or destruction of their personal data. For this purpose, we take a number of technical and organizational measures, such as:
+                </p>
+                <p>
+                    We use secure websites through which we collect your personal data;
+                </p>
+                <p>
+                    We collect your personal data through a two-step confirmation (entering data on our website and confirming the subscription, respectively your personal data, via a link sent to the email address you specified);
+                </p>
+                <p>
+                    We conduct reviews of our personal data collection, storage and processing practices, including physical security measures, to prevent unauthorized access to the systems we use;
+                </p>
+                <p>
+                    Access to your personal data within our organization is only available to persons whose work functions require access to the data in order to carry out the activities for which the data were collected; the data is processed or transmitted to third parties of which you are informed and which adhere to the highest standards of information security and security of personal data, demonstrated both by the presence of relevant certifications and by the possibility of carrying out regular audits for conformity.;
+                </p>
+            </div>
+        </section>
+    );
+}
