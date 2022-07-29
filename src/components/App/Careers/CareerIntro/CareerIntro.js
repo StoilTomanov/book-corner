d6399569f892d74e5699b0abf89232f1f6b1193b
StoilTomanov <stoiltomanov@gmail.com>
2022-07-29 16:17:24 +0300

    Careers sub components

---
+++ src/components/App/Careers/CareerIntro/CareerIntro.js
@@ -0,0 +1,11 @@
+export const CareerIntro = () => {
+    return (
+        <div className="careers-intro">
+            <img className="career-img" src="./assets/career_img.webp" alt="Girl with book" />
+            <div className="join-us">
+                <h2>Become one of us!</h2>
+                <p>Do you love books? How about we paying you to read any book you like? All you need to do is become part of our small big family 🙂 Keep scrolling!</p>
+            </div>
+        </div>
+    );
+}
