dd051adf9bfff7d1f6ae5daa0cc96d2a37bb1166
StoilTomanov <stoiltomanov@gmail.com>
2022-08-08 21:47:41 +0300

    profile tabs

---
+++ src/components/App/Profile/PersonalInfo/MyPlace.js
@@ -0,0 +1,12 @@
+export const MyPlace = () => {
+    return (
+        <div className="profile-content-wrapper">
+            <div className="myplace-content">
+                
+            </div>
+            <div className="info-content">
+
+            </div>
+        </div>
+    );
+}
